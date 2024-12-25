13833e57db64c9f180ae966af77f3aab72dc0510
hirateakiro <hirateakiro@gmail.com>
2024-12-25 18:46:54 +0900

    docs: フォームから送信された値を一括に代入するための処理に変更

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -22,10 +22,12 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
+        $inputs = $request->all();
+
         $todo = new Todo();
-        $todo->content = $content;
+        $todo->fill($inputs); // 変更
         $todo->save();
+
         return redirect()->route('todo.index');
     }
 }
