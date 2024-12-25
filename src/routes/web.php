668462b0056cafc12a96e8f7f371f344b4328a9e
hirateakiro <hirateakiro@gmail.com>
2024-12-25 17:17:06 +0900

    oDoがDBに新規作成されるように登録処理を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-Route::get('/todo', 'TodoController@index');
+Route::get('/todo', 'TodoController@index')->name('todo.index');
 
 Route::get('/todo/create', 'TodoController@create')->name('todo.create');
 
+Route::post('/todo', 'TodoController@store')->name('todo.store');
